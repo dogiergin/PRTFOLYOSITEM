54e08a5a7017cbd005d2e9e8e8f7104d2a29b852
dogiergin <edogukanergin@gmail.com>
2026-05-01 01:44:27 +0300

    Sertifikalar JPEG formatına çevrildi, PDF dosyaları ve parse_pdf klasörü silindi

--- read_cv.js
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-import pdf from 'pdf-parse/lib/pdf-parse.js';
-
-let dataBuffer = fs.readFileSync('CV (1).pdf');
-
-pdf(dataBuffer).then(function(data) {
-    console.log(data.text);
-}).catch(err => console.error(err));
